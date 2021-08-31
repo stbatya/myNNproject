44e58a036cef2ceb4875fefff63b10c1617a4932
k <punk.stum@gmail.com>
2021-08-31 17:11:01 +0300

    little mistake

--- NN/webapp/__init__.py
+++ NN/webapp/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from .models import User
 
 """app initialisation module"""
 
@@ -19,13 +18,18 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+    from .models import User
+
     #route for user loader.
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
+
     #register app to db
     db.init_app(app)
+
     #load blueprints
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
