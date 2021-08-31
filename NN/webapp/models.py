a5dd960c5f754c6b9fd737ce86d361f8b5668fdd
k <punk.stum@gmail.com>
2021-08-31 13:40:58 +0300

    some comments

--- NN/webapp/models.py
+++ NN/webapp/models.py
@@ -1,5 +1,8 @@
 from . import db
 from flask_login import UserMixin
+
+"""module contains ORM classes definitions for User and Result tables"""
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100),unique=True)
